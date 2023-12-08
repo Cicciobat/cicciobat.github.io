34f990a9d079f3a829509a3468db73f63a71bffd
Francesco Maria Battaglia <francescomariabattaglia@gmail.com>
2023-12-08 17:19:44 +0100

    feat(table): Implemented logic for table fill.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 $(() => {
+        let register = new Register();
+        let table = $('table tbody')
+
+
         if (!localStorage.getItem('students')) {
             fetch('../students.json')
                 .then((response) => response.json())
@@ -7,6 +11,28 @@ $(() => {
                     }
                 )
         }
+
+        for (const student of register.readStudents()) {
+            let markup = `<tr><td>${student.id}</td><td>${student.name}</td><td>${student.surname}</td><td ><i class="facilities fa-solid fa-pen-to-square"></i></td><td><i class="facilities fa-solid fa-trash"></i></td></tr>`
+            table.append(markup);
+        }
     }
 )
 
+class Register {
+
+    constructor() {
+    }
+
+    addStudent(student) {
+        let students = JSON.parse(localStorage.getItem('students'));
+
+        students.push(student)
+        localStorage.setItem('students', JSON.stringify(students));
+        return true;
+    }
+
+    readStudents() {
+        return JSON.parse(localStorage.getItem('students'));
+    }
+}
